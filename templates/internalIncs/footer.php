914fd4c5a3758743f74d9705fa062094bc13fe7f
FOURNIER Ghislain <theos10@users.noreply.github.com>
2019-10-04 22:39:19 +0200

    Update footer.php

--- templates/internalIncs/footer.php
+++ templates/internalIncs/footer.php
@@ -1,6 +1,5 @@
 	<footer class="text-center">
-		RaspiSMS a été créé pour vous par le site <a href="http://raspbian-france.fr">Raspbian-France</a>, site dédié à la Raspberry Pi<br/>
-		Copyright 2014. RaspiSMS est un programme sous <a href="https://www.gnu.org/licenses/gpl.txt" rel="nofollow">licence GNU GPL</a>.<br/>
+		RaspiSMS licence GNU GPL - Copyright 2014 by <a href="http://raspbian-france.fr">Raspbian-France</a><br/>
 	</footer>
 	<?php if (RASPISMS_SETTINGS_SMS_RECEPTION_SOUND) { ?>
 		<audio id="reception-sound">
