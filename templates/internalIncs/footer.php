379db16fe95e8a5892eafbddd73867374ac264b1
root <root@HunabKu>
2019-10-31 21:31:47 +0100

    Mise a jour responsive

--- templates/internalIncs/footer.php
+++ templates/internalIncs/footer.php
@@ -1,5 +1,6 @@
 	<footer class="text-center">
-		RaspiSMS licence GNU GPL - Copyright 2014 by <a href="http://raspbian-france.fr">Raspbian-France</a><br/>
+		RaspiSMS a été créé pour vous par le site <a href="http://raspbian-france.fr">Raspbian-France</a>, site dédié à la Raspberry Pi<br/>
+		Copyright 2014. RaspiSMS est un programme sous <a href="https://www.gnu.org/licenses/gpl.txt" rel="nofollow">licence GNU GPL</a>.<br/>
 	</footer>
 	<?php if (RASPISMS_SETTINGS_SMS_RECEPTION_SOUND) { ?>
 		<audio id="reception-sound">
--- templates/internalIncs/head.php
+++ templates/internalIncs/head.php
@@ -65,7 +65,6 @@
 				?>
 			});
 		</script>
-	    <meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body>
 	<div class="popup-alerts-container"></div>
